1bf7efa04bfa1640282673193267cacca87118da
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2024-08-26 07:57:19 -0500

    Adding constraint and is_kellogg features and RHEL7 and RHEL8 compatability

--- form.js
+++ form.js
@@ -50,7 +50,7 @@ function get_associations() {
   const assocs = [];
   // Obtain all unique partition and allocation combinations
   for (const assoc of raw_data.split(" ").filter(x => x)) {
-    const [PARTITION, GROUPS, TIMELIMIT, GRES, MEMORY, CPUS] = assoc.split("|");
+    const [PARTITION, GROUPS, TIMELIMIT, GRES, MEMORY, CPUS, FEATURE] = assoc.split("|");
     const groups = raw_groups_data.filter(value => GROUPS.includes(value));
     if (PARTITION.includes("a9009") && raw_groups_data.includes("a9009")) {
         assocs.push({ partition: PARTITION,
@@ -58,7 +58,8 @@ function get_associations() {
                       maxtime : convert_timelimit(TIMELIMIT),
                       gpus: GRES,
                       max_mem: MEMORY,
-                      max_cpus: CPUS});
+                      max_cpus: CPUS,
+                      feature: FEATURE});
     } else if (GROUPS.includes("all") && (general_access_allocations.length !== 0) && (PARTITION !== 'a9009') && (PARTITION !== 'buyin-dev')) {
         for (let gen_access of general_access_allocations) {
             assocs.push({ partition: PARTITION.replace('*', ''),
@@ -66,7 +67,8 @@ function get_associations() {
                           maxtime : convert_timelimit(TIMELIMIT),
                           gpus: GRES,
                           max_mem: MEMORY,
-                          max_cpus: CPUS});
+                          max_cpus: CPUS,
+                          feature: FEATURE});
         }
     } else if (groups.length !== 0) {
         for (const group of groups) {
@@ -75,7 +77,8 @@ function get_associations() {
                           maxtime : convert_timelimit(TIMELIMIT),
                           gpus: GRES,
                           max_mem: MEMORY,
-                          max_cpus: CPUS});
+                          max_cpus: CPUS,
+                          feature: FEATURE});
         }
     }
   }
@@ -92,6 +95,11 @@ function replace_options($select, new_options) {
   }
 }
 
+function reverse_options($select, new_options) {
+  $select.empty();
+  new_options.map(option => $select.append($("<option></option>").attr("value", option).text(option)));
+}
+
 /**
  *  Toggle the visibility of the GRES Value field
  */
@@ -103,6 +111,35 @@ function toggle_gres_value_field_visibility(assocs) {
     gpu_partitions.length !== 0);
 
   replace_options($("#batch_connect_session_context_gres_value"), convert_gpu_partitions(gpu_partitions));
+}
+
+/**
+ *  Toggle the visibility of the constraint value field
+ */
+function update_constraint_options(assocs) {
+  var constraints_with_commas = [...new Set(assocs.map(({ feature }) => feature))];
+  var constraints_without_commas = [""];
+  for (var constraint of constraints_with_commas) { constraints_without_commas.push(constraint.split(",")); }
+  replace_options($("#batch_connect_session_context_constraint"), [...new Set(constraints_without_commas.flat())]);
+}
+
+/**
+ *  If kellogg, set some things
+ */
+function is_kellogg() {
+  if ($("#batch_connect_session_context_slurm_partition").val() === 'kellogg') {
+    toggle_visibility_of_form_group(
+      '#batch_connect_session_context_request_more_than_one_node',
+      false);
+    $("#batch_connect_session_context_constraint").val("rhel8")
+    var gpu_options = [];
+    $("#batch_connect_session_context_gres_value option").each(function(index, item) { gpu_options.push(item.value) });
+    reverse_options($("#batch_connect_session_context_gres_value"), gpu_options.reverse());
+  } else {
+    toggle_visibility_of_form_group(
+      '#batch_connect_session_context_request_more_than_one_node',
+      true);
+  }
 }
 
 function toggle_number_of_nodes_visibility() {
@@ -167,6 +204,8 @@ function set_slurm_partition_change_handler() {
   slurm_partition.change(() => {
     let assocs = update_available_options();
     toggle_gres_value_field_visibility(assocs);
+    update_constraint_options(assocs);
+    is_kellogg()
     update_min_max(assocs);
   });
 }
@@ -211,11 +250,14 @@ function collapse_help() {
  *  Install event handlers
  */
 $(document).ready(function() {
+  // This sets all of the Slurm partitions for this user
   set_available_partitions();
   // Update available options appropriately
   let assocs = update_available_options();
   // Ensure that fields are shown or hidden based on what was set in the last session
   toggle_gres_value_field_visibility(assocs);
+  update_constraint_options(assocs);
+  is_kellogg();
   update_min_max(assocs);
   toggle_number_of_nodes_visibility();
   set_slurm_partition_change_handler();
